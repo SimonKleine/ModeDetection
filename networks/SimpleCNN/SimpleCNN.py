bdd2c9f689eb7c1ab28e8ff9d72b2990cec99c18
Simon Kleine <simon-kleine@web.de>
2019-02-07 14:29:20 +0100

    some progress the network accepts input

--- networks/SimpleCNN/SimpleCNN.py
+++ networks/SimpleCNN/SimpleCNN.py
@@ -8,6 +8,7 @@ import accelerometerfeatures.utils.pytorch.dataset as dataset
 
 import targetlabel_to_number
 
+EPOCH = 1
 
 class Network (nn.Module):
 
@@ -27,11 +28,26 @@ class Network (nn.Module):
 
 data = dataset.AccelerometerDatasetLoader(
     "/home/simonk/Documents/Bachelorarbeit"
-    "/data/accelerometer_data/1st_study.csv", perform_interpolation=True)
-data_windows_user1 = data.get_dataset_for_users(["a526f3566e9c9024dfa7378eb4291d787a09fd37"])
-tensor_data = torch.Tensor([window[0] for window in data_windows_user1])
-network = Network()
-network(tensor_data)
-test = targetlabel_to_number.targetlabel_to_number()
-test1 = test.getTargetMatrix(data_windows_user1)
-print(test1)
+    "/data/accelerometer_data/1st_study.csv",
+    perform_interpolation=True)
+train_windows = data.get_dataset_for_users(
+    ["a526f3566e9c9024dfa7378eb4291d787a09fd37"])
+train_windows_no_label = torch.Tensor(
+    [window[0] for window in train_windows])
+targetlabel_to_number = targetlabel_to_number.targetlabel_to_number()
+target_matrix = torch.Tensor(targetlabel_to_number.getTargetMatrix(train_windows))
+
+cnn = Network()
+optimizer = optim.Adam(cnn.parameters(), lr=0.1)
+loss_func = nn.CrossEntropyLoss()
+
+for epoch in range(EPOCH):
+    for step, input in enumerate(train_windows_no_label):
+       output = cnn(input.unsqueeze(0))
+       loss = loss_func(output, target_matrix[step])
+       optimizer.zero_grad()
+       loss.backward()
+       optimizer.step()
+
+
+
