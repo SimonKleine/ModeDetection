d5ecf30bf0ad5c6386a76bc702ce3c84e07099c4
Simon Kleine <simon-kleine@web.de>
2019-02-14 15:18:21 +0100

    added saving, loading and a bugged accuracy-module

--- networks/SimpleCNN/SimpleCNN.py
+++ networks/SimpleCNN/SimpleCNN.py
@@ -1,13 +1,15 @@
 #author Simon Kleine
+import os
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import accelerometerfeatures.utils.pytorch.dataset as dataset
+import numpy as np
 
 import targetlabel_to_number
 
-EPOCH = 1
+EPOCH = 3
 
 class Network (nn.Module):
 
@@ -27,12 +29,36 @@ class Network (nn.Module):
         return x
 
 
+def accuracy(cnn, users_valid):
+    train_windows = data.get_dataset_for_users(users_valid)
+    train_windows_no_label = torch.Tensor(
+        [window[0] for window in train_windows])
+    target_matrix1d = targetlabel_to_number.getTargetMatrix1d(
+        train_windows)
+    output_list = []
+    for step, input in enumerate(train_windows_no_label):
+        output = cnn(input.unsqueeze(0))
+        output = output.detach()
+        print(np.argmax(output))
+        output_list.append(np.argmax(output))
+    print(output_list)
+    same = sum(output_list == target_matrix1d)
+    not_same = sum(output_list != target_matrix1d)
+    acc = same / (same + not_same) * 100
+
+    return acc
+
+
 data = dataset.AccelerometerDatasetLoader(
     "/home/simonk/Documents/Bachelorarbeit"
     "/data/accelerometer_data/1st_study.csv",
     perform_interpolation=True)
-train_windows = data.get_dataset_for_users(
-    ["a526f3566e9c9024dfa7378eb4291d787a09fd37"])
+users = data.users
+users_train = users[0:len(users)-2]
+users_valid = users[len(users)-1: len(users)]
+
+
+train_windows = data.get_dataset_for_users(users_train)
 train_windows_no_label = torch.Tensor(
     [window[0] for window in train_windows])
 targetlabel_to_number = targetlabel_to_number.targetlabel_to_number()
@@ -40,9 +66,10 @@ target_matrix1d = targetlabel_to_number.getTargetMatrix1d(
                       train_windows)
 
 cnn = Network()
+#if os.path.isfile("cnn.pt"):
+#    cnn = torch.load("cnn.pt")
 optimizer = optim.Adam(cnn.parameters(), lr=0.1)
 loss_func = nn.CrossEntropyLoss()
-
 for epoch in range(EPOCH):
     for step, input in enumerate(train_windows_no_label):
        output = cnn(input.unsqueeze(0))
@@ -51,5 +78,5 @@ for epoch in range(EPOCH):
        loss.backward()
        optimizer.step()
 
-
-
+torch.save(cnn, "cnn.pt")
+print(accuracy(cnn, users_valid))
