77c5226bb3ebadeac7b7e0558f5c1caceb7a7571
Simon Kleine <simon-kleine@web.de>
2019-02-18 16:30:40 +0100

    added setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='ModeDetection',
+    version='0.1',
+    packages=[
+
+    ],
+    url='',
+    license='',
+    author='Simon Kleine',
+    author_email='',
+    description='',
+    install_requires=[
+        'numpy==1.15.4',
+        'torch==1.0.0',
+        'torchvision==0.2.1',
+    ]
+)
