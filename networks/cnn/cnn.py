43a5a614fb50b037490d332ffa52f2b7b7a4e186
SimonKleine <simon-kleine@web.de>
2019-03-14 18:15:40 +0100

    50 run kernel 7

--- networks/cnn/cnn.py
+++ networks/cnn/cnn.py
@@ -66,7 +66,7 @@ def get_accuracy(cnn, target_matrix_1d, train_windows_no_label):
 
 
 if __name__ == '__main__':
-    EPOCH = 50
+    EPOCH = 1
     overall_accuracy_list = []
     argparser = ArgumentParser()
     argparser.add_argument('training_data_file_path')
@@ -77,14 +77,16 @@ if __name__ == '__main__':
         args.training_data_file_path, perform_interpolation=True)
 
     users = data.users
-    logfile = open("logfilecnn_epoch=500.txt", "w")
+    logfile = open("logfilecnn_epoch=1.txt", "w")
     for current_user in users:
         users_train = users.copy()
         users_train.remove(current_user)
         users_valid = [current_user]
         print("User ", (len(overall_accuracy_list) + 1),
               " von ", len(users_train))
-
+        print(users)
+        print(users_train)
+        print(users_valid)
         print("Creating training windows..")
         train_windows = data.get_dataset_for_users(users_train)
         valid_windows = data.get_dataset_for_users(users_valid)
