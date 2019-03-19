8903362f508ef899a2485f3e4ebf111c6633198f
SimonKleine <simon-kleine@web.de>
2019-03-19 14:05:47 +0100

    new net

--- networks/cnn_gen2/cnngen2.py
+++ networks/cnn_gen2/cnngen2.py
@@ -102,7 +102,7 @@ if __name__ == '__main__':
         args.training_data_file_path, perform_interpolation=True)
 
     users = data.users
-    logfile = open("logfilecnn_epoch=1.txt", "w")
+    logfile = open("logfilecnn_epoch=30.txt", "w")
     for current_user in users:
         users_train = users.copy()
         users_train.remove(current_user)
