1ce932225a9aac65622fa58aaf86c01698500ab0
SimonKleine <simon-kleine@web.de>
2019-04-30 15:05:38 +0200

    next run

--- networks/cnn/cnn.py
+++ networks/cnn/cnn.py
@@ -30,7 +30,7 @@ class ConvolutionalNeuralNetwork (nn.Module):
         self.firstpoolinglayer = nn.MaxPool1d(kernel_size=2)
         self.secondconvolutionlayer = nn.Conv1d(18, 324, 5)
         self.secondpoolinglayer = nn.MaxPool1d(kernel_size=2)
-        self.firstlinearlayer = nn.Linear(116 * 18 * 18, 7)
+        self.firstlinearlayer = nn.Linear(37908, 7)
 
     def forward(self, x):
         '''
@@ -44,7 +44,7 @@ class ConvolutionalNeuralNetwork (nn.Module):
         x = self.firstpoolinglayer(x)
         x = self.secondconvolutionlayer(x)
         x = self.secondpoolinglayer(x)
-        x = x.view(1, 116 * 18 * 18)
+        x = x.view(1, 37908)
         x = self.firstlinearlayer(x)
         return x
 
