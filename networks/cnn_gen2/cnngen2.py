46f27be176a8ed78a172a8582132feb437e0c5ae
SimonKleine <simon-kleine@web.de>
2019-05-13 11:13:45 +0200

    avgpooling 7

--- networks/cnn_gen2/cnngen2.py
+++ networks/cnn_gen2/cnngen2.py
@@ -27,10 +27,10 @@ class ConvolutionalNeuralNetwork (nn.Module):
         self.thirdlayer = nn.Linear(37260, 7)
         '''
         self.firtconvolutionlayer = nn.Conv1d(3, 18, 4)
-        self.firstpoolinglayer = nn.AvgPool1d(kernel_size=3)
+        self.firstpoolinglayer = nn.AvgPool1d(kernel_size=7)
         self.secondconvolutionlayer = nn.Conv1d(18, 324, 4)
-        self.secondpoolinglayer = nn.AvgPool1d(kernel_size=3)
-        self.firstlinearlayer = nn.Linear(16848, 7)
+        self.secondpoolinglayer = nn.AvgPool1d(kernel_size=7)
+        self.firstlinearlayer = nn.Linear(10*18*18, 7)
      
 
 
@@ -46,7 +46,7 @@ class ConvolutionalNeuralNetwork (nn.Module):
         x = self.firstpoolinglayer(x)
         x = self.secondconvolutionlayer(x)
         x = self.secondpoolinglayer(x)
-        x = x.view(1, 16848)
+        x = x.view(1, 10*18*18)
         x = self.firstlinearlayer(x)
      
         return x
