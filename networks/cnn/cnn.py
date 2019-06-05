a2cbbfcffb14adf52b7379e99fa63184ba003516
SimonKleine <simon-kleine@web.de>
2019-06-05 17:19:59 +0200

    f

--- networks/cnn/cnn.py
+++ networks/cnn/cnn.py
@@ -112,8 +112,7 @@ def shuffle2(input):
             output.append(tail_train[x])
         for x in range(len(head_train)):
             output.append(head_train[x])
-        for x in output:
-            print(output[1][1])
+
     return output
 
 def get_accuracy_majorityvote(cnn, target_matrix_1d, train_windows_no_label):
@@ -211,6 +210,7 @@ if __name__ == '__main__':
             train_windows_int_shuffle = shuffle2(train_windows_int)
             train_windows_int_shuffle_29 = smaller(train_windows_int_shuffle)
             train_windows_int_shuffle_29 = shuffle2(train_windows_int_shuffle_29)
+            train_windows_int_shuffle_29 = train_windows_int_shuffle_29.cuda(
             #shufflearray = shuffle(train_windows_no_label, target_matrix_1d)
             #train_windows_no_label = shufflearray[0]
             #target_matrix_1d = shufflearray[1]
